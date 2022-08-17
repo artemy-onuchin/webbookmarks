d089e44eda785530be3191e1e47696b6f7b4571c
Artemy <artemonuchin@gmail.com>
2022-08-17 21:26:47 +0300

    Updated project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    let projectName = 'Web Bookmarks';
+
+    return (
+        <div className="App">
+            <h1>{projectName}</h1>
+        </div>
+    );
+}
+
+export default App;
