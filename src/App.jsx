e367dfbae764bdc9e9864acd4a5169dd75723af0
Artemy <artemonuchin@gmail.com>
2022-11-15 23:26:11 +0200

    Rebuild layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 
 import Header from './components/layout/Header';
 import Navigation from './components/layout/Navigation';
-import Card from './components/general/Card';
+import Content from './components/layout/Content';
 import Footer from './components/layout/Footer';
 import { Groups } from './data/data';
 
@@ -22,30 +22,7 @@ function App() {
         <div className="wrapper">
             <Header menuStatus={menuStatus} onClickBurger={() => setMenuStatus(!menuStatus)}/>
             <Navigation open={menuStatus} onClickTabs={tab => setActiveGroupId(tab)}/>
-            <main className="main">
-                <h1 className="card__list-name">Tab group #1</h1>
-                <div className="card__list">
-                    <Card name="YouTube"/>
-                    <Card name="Google"/>
-                    <Card name="Instagram"/>
-                    <Card name="Facebook"/>
-                    <Card name="Yahoo"/>
-                    <Card name="Microsoft"/>
-                    <Card name="Wikipedia"/>
-                    <Card name="Twitter"/>
-                    <Card name="Amazon"/>
-                    <Card name="Ebay"/>
-                    <Card name="Apple"/>
-                    <Card name="Paypal"/>
-                    <Card name="Rapidshare"/>
-                    <Card name="Adobe"/>
-                    <Card name="TikTok"/>
-                    <Card name="Minecraft"/>
-                    <Card name="Steam"/>
-                    <Card name="Dribbble"/>
-                    <Card name="Behance"/>
-                </div>
-            </main>
+            <Content active={activeGroupId}/>
             <Footer/>
         </div>
     );
