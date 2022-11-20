ceb6612c000ef6d24078e520d8875bc2f8848367
Artemy <artemonuchin@gmail.com>
2022-11-20 19:30:40 +0200

    Output cards on content area with active title

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,31 +1,41 @@
 import React from 'react';
 
 import Card from '../general/Card';
+import { Data, Groups } from '../../data/data';
 
-const Content = () => {
+const Content = ({active}) => {
+
+    let cardList = Data.map((item) => {
+        return(
+            <Card
+                key={item.id}
+                title={item.title}
+                url={item.url}
+                thumbnail={item.thumbnail}
+                thumbnailRemote={item.thumbnailRemote}
+                icon={item.icon}
+                group={item.group}
+                describtion={item.describtion}
+                active={active}
+                position={item.position}
+                idgroup={item.idgroup}
+            />
+        )
+    })
+
+    let groupTitle = Groups.map((item)=>{
+        return(
+            item.id === active && item.title
+        )
+    })
+
     return (
         <main className="main">
-            <h1 className="card__list-name">Tab group #1</h1>
+            <h1 className="card__list-name">
+                {groupTitle}
+            </h1>
             <div className="card__list">
-                <Card name="YouTube"/>
-                <Card name="Google"/>
-                <Card name="Instagram"/>
-                <Card name="Facebook"/>
-                <Card name="Yahoo"/>
-                <Card name="Microsoft"/>
-                <Card name="Wikipedia"/>
-                <Card name="Twitter"/>
-                <Card name="Amazon"/>
-                <Card name="Ebay"/>
-                <Card name="Apple"/>
-                <Card name="Paypal"/>
-                <Card name="Rapidshare"/>
-                <Card name="Adobe"/>
-                <Card name="TikTok"/>
-                <Card name="Minecraft"/>
-                <Card name="Steam"/>
-                <Card name="Dribbble"/>
-                <Card name="Behance"/>
+                {cardList}
             </div>
         </main>
     );
