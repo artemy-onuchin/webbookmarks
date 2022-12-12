973acca12143d30a84c85295a5f1886f4bafe3e7
Artemy <artemonuchin@gmail.com>
2022-12-12 22:31:43 +0200

    Update footer icons

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 
-import FooterTag from '../general/FooterTag'
+import FooterTag from '../general/FooterTag';
+import { FooterLinks } from '../../data/data';
 
 export default function Footer() {
     return (
         <footer className="footer">
-            <FooterTag name="Gmail"/>
-            <FooterTag name="Translate"/>
-            <FooterTag name="Photo"/>
-            <FooterTag name="GDrive"/>
-            <FooterTag name="Maps"/>
+            {FooterLinks.map((link, id) => {
+                return (
+                    link.id &&
+                    <FooterTag key={id} title={link.title} url={link.url} icon={link.icon} position={link.position}/>
+                )
+            })}
         </footer>
     )
 }
+
+
