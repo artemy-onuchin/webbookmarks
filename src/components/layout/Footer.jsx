09eff3e2621960de5b51bf358f308518b45d37a5
Artemy <artemonuchin@gmail.com>
2023-01-11 23:31:04 +0200

    Footer is done

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -16,12 +16,14 @@ export default function Footer() {
             </div>
 
             <div className="footer__copyright">
-                <div className="footer__copyright-react">
-                    Build on <a href="https://reactjs.org/" rel="noreferrer" target="_blank">React</a> with <a href="https://github.com/artemy-onuchin" rel="noreferrer" target="_blank">love</a>.
+                <div className="footer__wrapper">
+                    <div className="footer__copyright-react">
+                        Build on <a href="https://reactjs.org/" rel="noreferrer" target="_blank">React</a> with <a href="https://github.com/artemy-onuchin" rel="noreferrer" target="_blank">love</a>.
+                    </div>
+                    <a href="https://buymeacoffee.com/twy0voz" className="footer__copyright-coffee" rel="noreferrer" target="_blank">
+                        <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style={{height: '25px', width: '90px'}} />
+                    </a>
                 </div>
-                <a href="https://buymeacoffee.com/twy0voz" className="footer__copyright-coffee" rel="noreferrer" target="_blank">
-                    <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style={{height: '25px', width: '90px'}} />
-                </a>
             </div>
         </footer>
     )
