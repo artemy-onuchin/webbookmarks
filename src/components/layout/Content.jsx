6d42f89e8a7d0b38588698387edc0973f19027be
Artemy <artemonuchin@gmail.com>
2023-02-18 21:40:35 +0200

    Removed unused

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useState} from 'react';
 
 import Card from '../general/Card';
 import { Data, Groups } from '../../data/data';
